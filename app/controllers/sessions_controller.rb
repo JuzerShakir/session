ab53e387b37d8164f00d0ad1c497cfc4c6eecec5
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-10-31 15:44:43 +0530

    explain logic and updated flash message for wrong password input

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,31 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # finds visitor with corresponding email, returns nil if not found or else a hash
     visitor = Visitor.find_by_email(params[:email])
 
+    # if visitor is found and the password entered matches with the password in db..
     if visitor && visitor.authenticate(params[:password])
+      # ..then set the session
       session[:id] = visitor.id
       flash[:success] = "Welcome Back! A session has been created."
+      # home page
       redirect_to :root
 
+    # if any input fields are blank
     elsif params[:email].blank? || params[:password].blank?
       flash[:error] = "Fields cannot be empty!"
       render :new
 
+    # if email enetered is not in our database
     elsif visitor == nil
       flash[:error] = "Email doesn't exist! Please Sign up!"
+      # .. then redirect them to signup page
       redirect_to :new_visitor
+
+    # or else entered password is wrong
     else
-      flash[:error] = "Password is wrong! Retry!"
+      flash[:error] = "Email exists but the password entered is wrong! Retry!"
       render :new
     end
   end
