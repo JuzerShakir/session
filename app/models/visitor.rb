c9f19d050bc9f5e0991d6421a18597906e6692fb
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-10-31 10:32:11 +0530

    Remove confirmation validation as bcrypt provides one

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,7 +5,7 @@ class Visitor < ApplicationRecord
   # only valid email format allowed
   validates :email, format: { with: /\A[^@\s]+@(?:[-a-z0-9]+\.)+[a-z]{2,}\z/i, message: "Invalid email format!"}, uniqueness: true
 
-  # setting length of password and passowrd confirmation
-  validates :password, length: { minimum: 6, maximum: 36 }, confirmation: true
+  # setting length of password
+  validates :password, length: { minimum: 6, maximum: 36 }
   validates :password_confirmation, presence: true
 end
