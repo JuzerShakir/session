cbcbff2d1c442d32b8a4276b8b0b0154f4bba35f
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-01 19:00:58 +0530

    Fix- Now refreshing signup page with errors will not show page not found. Routes updated!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'visitors#index'
 
-  resources :visitors, only: [:new, :create]
+  get '/signup', to: 'visitors#new', as: 'signup'
+  post '/signup', to: 'visitors#create'
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
