5ea03083e2ab85e6548e5f7fc1c556952de83b46
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-02 00:16:55 +0530

    expect contents to be in the specified html element

--- spec/views/visitors/new_spec.rb
+++ spec/views/visitors/new_spec.rb
@@ -14,8 +14,10 @@ RSpec.feature "When creating visitor account" do
         fill_in "password", with: "#{@new_visitor.password}"
         fill_in "password_confirmation", with: "#{@new_visitor.password_confirmation}"
         click_button "Create Visitor"
-        expect(page).to have_content("Congrats! You have successfully logged in and a session has been created.")
         expect(page.current_path).to eq(root_path)
+        within "div.alert" do
+            expect(page).to have_content("Congrats! You have successfully logged in and a session has been created.")
+        end
     end
 
     scenario "all fields should be filled" do
@@ -24,15 +26,20 @@ RSpec.feature "When creating visitor account" do
         fill_in "password_confirmation", with: ""
         click_button "Create Visitor"
         expect(page.current_path).to eq(signup_path)
-        expect(page).to have_content("Password can't be blank")
-        expect(page).to have_content("Email is in nvalid format!")
-        expect(page).to have_content("Password is too short (minimum is 6 characters)")
+
+        within "div#error_explanation" do
+            expect(page).to have_content("Password can't be blank")
+            expect(page).to have_content("Email is in nvalid format!")
+            expect(page).to have_content("Password is too short (minimum is 6 characters)")
+        end
     end
 
     scenario "an existing user cannot sign up again" do
         fill_in "email", with: "#{@existing_visitor.email}"
         click_button "Create Visitor"
         expect(page.current_path).to eq(login_path)
-        expect(page).to have_content("#{@existing_visitor.email} is already in use! You can Log In with this email.")
+        within "div.alert" do
+            expect(page).to have_content("#{@existing_visitor.email} is already in use! You can Log In with this email.")
+        end
     end
 end
