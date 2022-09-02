0a1be217fdbf7007b8408cdc301472feab9f3752
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-02 09:48:10 +0530

    On login page: when new user logs in: success

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "On login page" do
         visit login_path
         @existing_visitor = Visitor.create(email: "ext_user@aol.com",
             password: "123456", password_confirmation: "123456")
+        @new_visitor = Visitor.new(email: "new_user@yahoo.com", password: "123456")
     end
     scenario "when existing user logs in" do
         fill_in "email", with: @existing_visitor.email
@@ -15,6 +16,13 @@ RSpec.feature "On login page" do
         within "div.alert" do expect(page).to have_content("Welcome Back! A session has been created.") end
         expect(page).to have_content(@existing_visitor.email)
     end
-    scenario "when new user logs in"
+
+    scenario "when new user logs in" do
+        fill_in "email", with: @new_visitor.email
+        fill_in "password",	with: @new_visitor.password
+        click_button "Sign In"
+        expect(page.current_path).to eq(signup_path)
+        within "div.alert" do expect(page).to have_content("Email doesn't exist! Please Sign up!") end
+    end
     scenario "if fields are empty"
 end
